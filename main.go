304f2cc806396184a5614af76896e62ef808ab4d
Pushka1234 <pavelk.cherepanov@gmail.com>
2020-12-29 22:07:13 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
+func index(w http.ResponseWriter, r *http.Request){
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil{
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "index", nil)
+}
+
+
+func handleFunc(){
+	http.HandleFunc("/", index)
+	http.ListenAndServe("127.0.0.1:8080", nil)
+}
+
+func main(){
+	handleFunc()
+}
