d478888a41f2b34373ccac8bb1ff9d00d4950229
PavelCherepanov <pavelk.cherepanov@gmail.com>
2020-12-31 00:26:40 +0700

    third commit

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"fmt"
 	"net/http"
 	"html/template"
@@ -26,10 +28,36 @@ func create(w http.ResponseWriter, r *http.Request){
 	t.ExecuteTemplate(w, "create", nil)
 }
 
+func save_article(w http.ResponseWriter, r *http.Request){
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+
+	if title == "" || anons == "" || full_text == ""{
+		fmt.Fprintf(w, "Не все данные заполнены")
+	} else{
+
+		db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+		if err != nil{
+			panic(err)
+		}
+		defer db.Close()
+
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES ('%s', '%s', '%s')", title, anons, full_text))
+		if err != nil{
+			panic(err)
+		}
+		defer insert.Close()
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
 func handleFunc(){
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	http.HandleFunc("/", index)
 	http.HandleFunc("/create", create)
+	http.HandleFunc("/save_article", save_article)
 	http.ListenAndServe("127.0.0.1:8008", nil)
 }
 
